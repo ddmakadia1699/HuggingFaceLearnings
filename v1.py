a0f19a9520a3143f6635bdb2125bd450670b0991
ddmakadia1699 <38184843+ddmakadia1699@users.noreply.github.com>
2024-09-08 19:10:27 +0530

    Initial commit

---
+++ v1.py
@@ -0,0 +1,44 @@
+from transformers import pipeline
+
+# classifier = pipeline('sentiment-analysis')
+# print('-----------------')
+# print(classifier(["I've been waiting for a huggingface course my whole life.",
+#                   "I hate this so much."]))
+
+# classifier = pipeline('zero-shot-classification')
+# print('-----------------')
+# print(classifier(
+#     "this course is about the transformers library",
+#     candidate_labels = ["eduction", "politics" ,"business"]))
+
+# classifier = pipeline('text-generation')
+# print('-----------------')
+# print(classifier("in this course, we will teach you how to"))
+
+# classifier = pipeline('text-generation' , model = 'distilgpt2')
+# print('-----------------')
+# print(classifier("in this course, we will teach you how to",
+#                  max_length = 30,
+#                  num_return_sequences = 5)
+#     )
+
+# classifier = pipeline('fill-mask')
+# print('-----------------')
+# print(classifier("this course will teach you all about <mask> models.",
+#                  top_k = 2)
+#     )
+
+# classifier = pipeline('ner', grouped_entities = True)
+# print('-----------------')
+# print(classifier("My name is Sylvain and I work at Hugging Face in Brooklyn."))
+
+# classifier = pipeline('question-answering')
+# print('-----------------')
+# print(classifier(
+#     question="where do i work?",
+#     context="My name is Sylvain and I work at Hugging Face in Brooklyn."))
+
+classifier = pipeline('summarization')
+print('-----------------')
+print(classifier("""
+    My name is Sylvain and I work at Hugging Face in Brooklyn."""))
